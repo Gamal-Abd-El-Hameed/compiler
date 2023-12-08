38c958ea76489a1b9ee856038c39edcde6f9b4f7
Salah-Eltenehy <91028700+Salah-Eltenehy@users.noreply.github.com>
2023-12-08 18:03:19 +0200

    add documentation

--- minimize.h
+++ minimize.h
@@ -9,16 +9,16 @@
 #include "SetStates.h"
 #include "DFA.h"
 
-
 #ifndef DFA_DFAMINIMIZED_H
 #define DFA_DFAMINIMIZED_H
 
-
+// Map to keep track of the group to 
+// which each state belongs during the minimization process.
 map<string, int> stateToGroupMap;
 
 /**
- * loop on the groups and fill the map
-*/
+ * Loop through the groups and update the stateToGroupMap.
+ */
 void updateStateToGroupMap(vector<vector<SetStates*>> groups) {
     stateToGroupMap.clear();
     for (int groupNumber = 0; groupNumber < groups.size(); ++groupNumber) {
@@ -28,36 +28,46 @@ void updateStateToGroupMap(vector<vector<SetStates*>> groups) {
     }
 }
 
-
-vector<vector<SetStates*>> minimizeDFA (vector<SetStates*> remStates, vector<SetStates*> finalStates){
+/**
+ * Minimize the DFA represented by the given remaining and final states.
+ * @param remStates: Remaining states
+ * @param finalStates: Final states
+ * @return: Vector of vectors representing the minimized groups of states.
+ */
+vector<vector<SetStates*>> minimizeDFA(vector<SetStates*> remStates, vector<SetStates*> finalStates) {
     const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&',.:;<=>?@~(){}[]*/+-|";
-    int previousGroupCount = 0 ;
-    vector<vector<SetStates*>> groups {std::move(remStates),std::move(finalStates)};
+    int previousGroupCount = 0;
+    vector<vector<SetStates*>> groups {std::move(remStates), std::move(finalStates)};
     updateStateToGroupMap(groups);
 
-    while(previousGroupCount != groups.size()) {
-        //get the number of partitions in this iteration
-        previousGroupCount = (int)groups.size();
+    while (previousGroupCount != groups.size()) {
+        // Get the number of partitions in this iteration.
+        previousGroupCount = static_cast<int>(groups.size());
         vector<vector<SetStates*>> newGroups;
+
+        // Iterate through each group.
         for (const auto& group : groups) {
-            // base case if this group is only one element
+            // Base case: if this group has only one element, add it to the new groups.
             if (group.size() == 1) {
                 newGroups.push_back(group);
                 continue;
             }
+
             vector<SetStates*> identicalStates, remainingStates;
             auto firstState = group[0];
             identicalStates.push_back(firstState);
-            // loop to fill identicalStates and remainingStates vectors
-            for (int i = 1 ; i < group.size() ; i++) {
-                // currentState is the state that we are comparing with firstState
+
+            // Loop to fill identicalStates and remainingStates vectors.
+            for (int i = 1; i < group.size(); i++) {
                 auto currentState = group[i];
                 bool isDifferent = false;
+
+                // Compare transitions for each input symbol in the alphabet.
                 for (char input : alphabet) {
                     bool hasTransition1 = (firstState->nextStates.find(input) != firstState->nextStates.end());
                     bool hasTransition2 = (currentState->nextStates.find(input) != currentState->nextStates.end());
-                    // if the 2 states are different
-                    // or if the 2 states have transition but to different groups
+
+                    // If the two states are different or have transitions to different groups, add to remainingStates.
                     if (hasTransition1 != hasTransition2 ||
                         (hasTransition1 &&
                          (stateToGroupMap[firstState->nextStates[input]->getStatesIds()] !=
@@ -67,41 +77,51 @@ vector<vector<SetStates*>> minimizeDFA (vector<SetStates*> remStates, vector<Set
                         break;
                     }
                 }
-                //if the 2 states are identicalStates
-                if(!isDifferent)
+
+                // If the two states are identical, add to identicalStates.
+                if (!isDifferent)
                     identicalStates.push_back(currentState);
             }
+
             newGroups.push_back(identicalStates);
-            if(!remainingStates.empty())
+            if (!remainingStates.empty())
                 newGroups.push_back(remainingStates);
         }
+
         groups = newGroups;
         updateStateToGroupMap(groups);
     }
+
     return groups;
 }
 
-
+/**
+ * Generate the minimized DFA based on the final groups of states.
+ * @param finalGroups: Vector of vectors representing the minimized groups of states.
+ * @return: Pointer to the minimized DFA.
+ */
 DFA* generateMinDFA(vector<vector<SetStates*>> finalGroups) {
     const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&',.:;<=>?@~(){}[]*/+-|";
     vector<SetStates*> minimizedStates;
 
     // Concatenate the groups together.
-    // loop on the groups
     for (const auto& group : finalGroups) {
         auto* setStates = new SetStates();
-        // loop on the states in the group
+
+        // Loop through the states in the group and insert them into the set.
         for (SetStates* setState : group) {
             setStates->insertSet(setState->states);
         }
-        minimizedStates.push_back(setStates) ;
+
+        minimizedStates.push_back(setStates);
     }
 
-    // get the next state for each state
+    // Assign next states for each state in the minimized DFA.
     for (int i = 0; i < finalGroups.size(); i++) {
         SetStates* representativeState = finalGroups[i][0];
-        for(char input:alphabet) {
-            // if the representativeState has transition with this input
+
+        for (char input : alphabet) {
+            // Check if the representativeState has a transition with this input.
             if (representativeState->nextStates.find(input) != representativeState->nextStates.end()) {
                 string outputStateId = representativeState->nextStates[input]->getStatesIds();
                 minimizedStates[i]->nextStates.insert({input, minimizedStates[stateToGroupMap[outputStateId]]});
@@ -109,11 +129,15 @@ DFA* generateMinDFA(vector<vector<SetStates*>> finalGroups) {
         }
     }
 
+    // Create the minimized DFA.
     DFA* minimizedDFA = new DFA();
     minimizedDFA->startingState = minimizedStates[0];
-    for (SetStates* states:minimizedStates) {
+
+    // Add states to the minimized DFA.
+    for (SetStates* states : minimizedStates) {
         minimizedDFA->stateIdToSetStatesMap.insert({states->getStatesIds(), states});
     }
+
     return minimizedDFA;
 }
 
